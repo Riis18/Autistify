22f8050a442c68bcbe19474cfa5ee2101dd2bbf8
Riis18 <Riisjesper@hotmail.com>
2017-11-30 10:23:31 +0100

    Fix

--- Autistify/src/autistify/be/Song.java
+++ Autistify/src/autistify/be/Song.java
@@ -11,6 +11,115 @@ package autistify.be;
  */
 public class Song {
     
-   
+       public String name;
+       public String album;
+       public String artist;
+       public String path;
+       private int trackLenght;
+       
+
+    public Song(String name, String album, String artist, String path, int trackLenght) {
+        this.name = name;
+        this.album = album;
+        this.artist = artist;
+        this.path = path;
+        this.trackLenght = trackLenght;
+    }
+
+    /**
+     * Get the value of trackLenght
+     *
+     * @return the value of trackLenght
+     */
+    public int getTrackLenght() {
+        return trackLenght;
+    }
+
+    /**
+     * Set the value of trackLenght
+     *
+     * @param trackLenght new value of trackLenght
+     */
+    public void setTrackLenght(int trackLenght) {
+        this.trackLenght = trackLenght;
+    }
+
+
+    /**
+     * Get the value of path
+     *
+     * @return the value of path
+     */
+    public String getPath() {
+        return path;
+    }
+
+    /**
+     * Set the value of path
+     *
+     * @param path new value of path
+     */
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+
+    /**
+     * Get the value of album
+     *
+     * @return the value of album
+     */
+    public String getAlbum() {
+        return album;
+    }
+
+    /**
+     * Set the value of album
+     *
+     * @param album new value of album
+     */
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    
+
+    /**
+     * Get the value of artist
+     *
+     * @return the value of artist
+     */
+    public String getArtist() {
+        return artist;
+    }
+
+    /**
+     * Set the value of artist
+     *
+     * @param artist new value of artist
+     */
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+
+    /**
+     * Get the value of name
+     *
+     * @return the value of name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @param name new value of name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
     
 }
--- Autistify/src/autistify/dal/DataBaseConnecter.java
+++ Autistify/src/autistify/dal/DataBaseConnecter.java
@@ -5,10 +5,34 @@
  */
 package autistify.dal;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import java.io.IOException;
+import java.sql.Connection;
+
 /**
  *
  * @author Jesper Riis
  */
 public class DataBaseConnecter {
+    
+    private SQLServerDataSource dataSource;
+
+    public DataBaseConnecter() throws IOException
+    {
+        dataSource = new SQLServerDataSource();
+
+        dataSource.setServerName("EASV-DB2");
+        dataSource.setPortNumber(1433);
+        dataSource.setDatabaseName("Autistify");
+        dataSource.setUser("CS2017A_15");
+        dataSource.setPassword("1234");
+    }
+    
+    public Connection getConnection() throws SQLServerException
+    {
+        return dataSource.getConnection();
+    }
+
     
 }
