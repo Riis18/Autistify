87ec0f25b6f94e2b805d51f82bde20652300b0de
Riis18 <Riisjesper@hotmail.com>
2017-12-05 09:32:08 +0100

    add song view added

--- Autistify/src/autistify/gui/controller/MainViewController.java
+++ Autistify/src/autistify/gui/controller/MainViewController.java
@@ -15,6 +15,10 @@ import autistify.dal.SongDAO;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -32,7 +36,7 @@ public class MainViewController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         try {
-            // TODO
+           
             sDAO = new SongDAO();
         } catch (IOException ex) {
             Logger.getLogger(MainViewController.class.getName()).log(Level.SEVERE, null, ex);
@@ -40,8 +44,18 @@ public class MainViewController implements Initializable {
     }    
 
     @FXML
-    private void addPik(ActionEvent event) {
-        sDAO.createSong(song);
+    private void openNewSongView(ActionEvent event) throws IOException {
+        
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/autistify/gui/view/AddSongView.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+//        AddSongViewController controller = fxmlLoader.getController();
+//        controller.setModel(songmodel);
+//        controller.setManager(playlistManager);
+        Stage stage = new Stage();
+        stage.setTitle("Add Song Window");
+        stage.setScene(new Scene(root1));
+        stage.show();
+        
     }
     
 }
